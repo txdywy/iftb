f6b0036b13d549cb5d099c4ee0c7fbae021ac63c
ROn <geniusron@gmail.com>
2026-05-06 21:18:56 +0800

    fix pages asset base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
+  base: './',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 700,
